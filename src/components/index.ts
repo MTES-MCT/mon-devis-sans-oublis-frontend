0937707ec0318d19d930e28eacf099cf06bd98e5
Samir BENFARES <sbenfares@gmail.com>
2025-05-26 15:03:00 +0200

    feat: refacto components & design

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,103 +1,98 @@
 // Alert
-export { default as Alert, AlertType } from './Alert/Alert';
-export type { AlertProps } from './Alert/Alert';
+export { default as Alert, AlertType } from "./Alert/Alert";
+export type { AlertProps } from "./Alert/Alert";
 // Badge
-export { default as Badge, BadgeVariant, BadgeSize } from './Badge/Badge';
-export type { BadgeProps } from './Badge/Badge';
+export { default as Badge, BadgeVariant, BadgeSize } from "./Badge/Badge";
+export type { BadgeProps } from "./Badge/Badge";
 // BlockNumber
 export {
   default as BlockNumber,
   BlockNumberSize,
-} from './BlockNumber/BlockNumber';
-export type { BlockNumberProps } from './BlockNumber/BlockNumber';
+} from "./BlockNumber/BlockNumber";
+export type { BlockNumberProps } from "./BlockNumber/BlockNumber";
 // Card
-export { default as Card } from './Card/Card';
-export type { CardProps } from './Card/Card';
+export { default as Card } from "./Card/Card";
+export type { CardProps } from "./Card/Card";
 // CardImage
-export { default as CardImage } from './CardImage/CardImage';
-export type { CardImageProps } from './CardImage/CardImage';
+export { default as CardImage } from "./CardImage/CardImage";
+export type { CardImageProps } from "./CardImage/CardImage";
 // CardLinkProfile
-export { default as CardLinkProfile } from './CardLinkProfile/CardLinkProfile';
-export type { CardLinkProfileProps } from './CardLinkProfile/CardLinkProfile';
+export { default as CardLinkProfile } from "./CardLinkProfile/CardLinkProfile";
+export type { CardLinkProfileProps } from "./CardLinkProfile/CardLinkProfile";
 // CheckboxGroup
-export { default as CheckboxGroup } from './CheckboxGroup/CheckboxGroup';
-export type { CheckboxGroupProps } from './CheckboxGroup/CheckboxGroup';
+export { default as CheckboxGroup } from "./CheckboxGroup/CheckboxGroup";
+export type { CheckboxGroupProps } from "./CheckboxGroup/CheckboxGroup";
 // Confetti
-export { default as Confetti } from './Confetti/Confetti';
+export { default as Confetti } from "./Confetti/Confetti";
 // DropdownCheckboxList
-export { default as DropdownCheckboxList } from './DropdownCheckboxList/DropdownCheckboxList';
-export type { DropdownCheckboxListProps } from './DropdownCheckboxList/DropdownCheckboxList';
+export { default as DropdownCheckboxList } from "./DropdownCheckboxList/DropdownCheckboxList";
+export type { DropdownCheckboxListProps } from "./DropdownCheckboxList/DropdownCheckboxList";
 // Footer
-export { default as Footer } from './Footer/Footer';
-export type { FooterProps } from './Footer/Footer';
+export { default as Footer } from "./Footer/Footer";
+export type { FooterProps } from "./Footer/Footer";
 // Header
-export { default as Header } from './Header/Header';
-export type { HeaderProps } from './Header/Header';
+export { default as Header } from "./Header/Header";
+export type { HeaderProps } from "./Header/Header";
 // IconBackground
 export {
   default as IconBackground,
   IconBackgroundVariant,
-} from './IconBackground/IconBackground';
-export type { IconBackgroundProps } from './IconBackground/IconBackground';
+} from "./IconBackground/IconBackground";
+export type { IconBackgroundProps } from "./IconBackground/IconBackground";
 // Link
-export { default as Link, LinkSize, LinkVariant } from './Link/Link';
-export type { LinkProps } from './Link/Link';
+export { default as Link, LinkSize, LinkVariant } from "./Link/Link";
+export type { LinkProps } from "./Link/Link";
 // LoadingDots
-export { default as LoadingDots } from './LoadingDots/LoadingDots';
-export type { LoadingDotsProps } from './LoadingDots/LoadingDots';
+export { default as LoadingDots } from "./LoadingDots/LoadingDots";
+export type { LoadingDotsProps } from "./LoadingDots/LoadingDots";
 // Matomo
-export { default as Matomo } from './Matomo/Matomo';
+export { default as Matomo } from "./Matomo/Matomo";
 // Modal
-export { default as Modal, ModalPosition } from './Modal/Modal';
-export type { ModalProps } from './Modal/Modal';
+export { default as Modal, ModalPosition } from "./Modal/Modal";
+export type { ModalProps } from "./Modal/Modal";
 // Modal - CommentErrorModal
-export { default as CommentErrorModal } from './Modal/CommentErrorModal/CommentErrorModal';
-export type { CommentErrorModalProps } from './Modal/CommentErrorModal/CommentErrorModal';
+export { default as CommentErrorModal } from "./Modal/CommentErrorModal/CommentErrorModal";
+export type { CommentErrorModalProps } from "./Modal/CommentErrorModal/CommentErrorModal";
 // Modal - DeleteErrorModal
-export { default as DeleteErrorModal } from './Modal/DeleteErrorModal/DeleteErrorModal';
-export type { DeleteErrorModalProps } from './Modal/DeleteErrorModal/DeleteErrorModal';
+export { default as DeleteErrorModal } from "./Modal/DeleteErrorModal/DeleteErrorModal";
+export type { DeleteErrorModalProps } from "./Modal/DeleteErrorModal/DeleteErrorModal";
 // Modal - ErrorDetailsModal
-export { default as ErrorDetailsModal } from './Modal/ErrorDetailsModal/ErrorDetailsModal';
-export type { ErrorDetailsModalProps } from './Modal/ErrorDetailsModal/ErrorDetailsModal';
+export { default as ErrorDetailsModal } from "./Modal/ErrorDetailsModal/ErrorDetailsModal";
+export type { ErrorDetailsModalProps } from "./Modal/ErrorDetailsModal/ErrorDetailsModal";
 // Modal - GlobalCommentModal
-export { default as GlobalCommentModal } from './Modal/GlobalCommentModal/GlobalCommentModal';
-export type { GlobalCommentModalProps } from './Modal/GlobalCommentModal/GlobalCommentModal';
+export { default as GlobalCommentModal } from "./Modal/GlobalCommentModal/GlobalCommentModal";
+export type { GlobalCommentModalProps } from "./Modal/GlobalCommentModal/GlobalCommentModal";
 // Modal - GlobalErrorFeedbacksModal
-export { default as GlobalErrorFeedbacksModal } from './Modal/GlobalErrorFeedbacksModal/GlobalErrorFeedbacksModal';
-export type { GlobalErrorFeedbacksModalProps } from './Modal/GlobalErrorFeedbacksModal/GlobalErrorFeedbacksModal';
+export { default as GlobalErrorFeedbacksModal } from "./Modal/GlobalErrorFeedbacksModal/GlobalErrorFeedbacksModal";
+export type { GlobalErrorFeedbacksModalProps } from "./Modal/GlobalErrorFeedbacksModal/GlobalErrorFeedbacksModal";
 // Notice
-export { default as Notice } from './Notice/Notice';
-export type { NoticeProps } from './Notice/Notice';
+export { default as Notice } from "./Notice/Notice";
+export type { NoticeProps } from "./Notice/Notice";
 // QuoteErrorLine
-export { default as QuoteErrorLine } from './QuoteErrorLine/QuoteErrorLine';
-export type { QuoteErrorLineProps } from './QuoteErrorLine/QuoteErrorLine';
+export { default as QuoteErrorLine } from "./QuoteErrorLine/QuoteErrorLine";
+export type { QuoteErrorLineProps } from "./QuoteErrorLine/QuoteErrorLine";
 // QuoteErrorTable
-export { default as QuoteErrorTable } from './QuoteErrorTable/QuoteErrorTable';
-export type { QuoteErrorTableProps } from './QuoteErrorTable/QuoteErrorTable';
-// QuoteStatusLink
-export {
-  default as QuoteStatusLink,
-  QuoteStatusType,
-} from './QuoteStatusLink/QuoteStatusLink';
-export type { QuoteStatusLinkProps } from './QuoteStatusLink/QuoteStatusLink';
+export { default as QuoteErrorTable } from "./QuoteErrorTable/QuoteErrorTable";
+export type { QuoteErrorTableProps } from "./QuoteErrorTable/QuoteErrorTable";
+
 // RoundCheckboxGroup
-export { default as RoundCheckboxGroup } from './RoundCheckboxGroup/RoundCheckboxGroup';
+export { default as RoundCheckboxGroup } from "./RoundCheckboxGroup/RoundCheckboxGroup";
 export type {
   CheckboxOption,
   RoundCheckboxGroupProps,
-} from './RoundCheckboxGroup/RoundCheckboxGroup';
+} from "./RoundCheckboxGroup/RoundCheckboxGroup";
 // SvgLoader
-export { default as SVGLoader } from './SvgLoader/SvgLoader';
-export type { SVGLoaderProps } from './SvgLoader/SvgLoader';
+export { default as SVGLoader } from "./SvgLoader/SvgLoader";
+export type { SVGLoaderProps } from "./SvgLoader/SvgLoader";
 // Tile
-export { default as Tile } from './Tile/Tile';
-export type { TileProps } from './Tile/Tile';
+export { default as Tile } from "./Tile/Tile";
+export type { TileProps } from "./Tile/Tile";
 // Toast
-export { default as Toast } from './Toast/Toast';
-export type { ToastProps } from './Toast/Toast';
+export { default as Toast } from "./Toast/Toast";
+export type { ToastProps } from "./Toast/Toast";
 // Tooltip
-export { default as Tooltip } from './Tooltip/Tooltip';
-export type { TooltipProps } from './Tooltip/Tooltip';
+export { default as Tooltip } from "./Tooltip/Tooltip";
+export type { TooltipProps } from "./Tooltip/Tooltip";
 // Upload
-export { default as Upload } from './Upload/Upload';
-export type { UploadProps } from './Upload/Upload';
+export { default as Upload } from "./Upload/Upload";
+export type { UploadProps } from "./Upload/Upload";
