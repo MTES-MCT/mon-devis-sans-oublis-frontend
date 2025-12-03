13a373ad6fe0e79ee509ec3452e189032c2f401a
nicolasleger <nicolasleger@users.noreply.github.com>
2025-12-03 16:09:55 +0100

    feat: split ExplanationCards in HowItWork and WhatFor, plus ProfileCards

--- src/page-sections/home/HowItWorks.tsx
+++ src/page-sections/home/HowItWorks.tsx
@@ -4,47 +4,24 @@ import { Badge, BadgeVariant, BlockNumber, CardImage, Link } from "@/components"
 import { richTextParser } from "@/utils";
 import wording from "@/wording";
 
-export default function ExplanationCards() {
+export default function HowItWorks() {
   return (
     <section className="fr-container-fluid fr-py-10w">
       <div className="fr-container">
-        <h2>{wording.homepage.explanation_cards.title_1}</h2>
-        <h5 className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">{wording.homepage.explanation_cards.subtitle_1}</h5>
-        <ul className="fr-raw-list fr-badges-group fr-mb-3w flex flex-wrap gap-4">
-          {wording.homepage.explanation_cards.badges_1.map((badge, index) => (
-            <li key={index}>
-              <Badge
-                label={badge.label}
-                variant={BadgeVariant.BLUE_DARK}
-              />
-            </li>
-          ))}
-        </ul>
-        <div className="fr-grid-row fr-grid-row--gutters flex flex-col md:flex-row">
-          {wording.homepage.explanation_cards.image_cards.map((card, index) => (
-            <div className="fr-col-12 fr-col-md-4 flex-1" key={index}>
-              <CardImage
-                description={richTextParser(card.description)}
-                image={card.image}
-                title={card.title}
-              />
-            </div>
-          ))}
-        </div>
         <h2 className="fr-mt-8w fr-mb-5w">
-          {wording.homepage.explanation_cards.title_3}
+          {wording.homepage.how_it_works.title}
         </h2>
         <div className="flex flex-col md:flex-row gap-10">
           <Image
-            alt={wording.homepage.explanation_cards.image.alt}
+            alt={wording.homepage.how_it_works.image.alt}
             className="w-auto lg:h-[420px] md:h-[380px] h-[224px] object-contain"
             height={0}
             sizes="(min-width: 1024px) 420px, (min-width: 768px) 380px, 224px"
-            src={wording.homepage.explanation_cards.image.src}
+            src={wording.homepage.how_it_works.image.src}
             width={0}
           />
           <div className="flex flex-col">
-            {wording.homepage.explanation_cards.number_blocks.map(
+            {wording.homepage.how_it_works.number_blocks.map(
               (block, index) => (
                 <BlockNumber
                   className="fr-mb-2w"
@@ -56,7 +33,7 @@ export default function ExplanationCards() {
             )}
             <p className="bg-[var(--background-alt-blue-france)] pl-2 p-4">
               <span className="fr-icon-restart-line mr-2 ml-0 text-[var(--text-title-blue-france)]" />
-              {wording.homepage.explanation_cards.correction}
+              {wording.homepage.how_it_works.correction}
             </p>
             <div className="flex items-end md:mt-auto mt-6 justify-center md:justify-start">
               <Link {...wording.homepage.check_quote_button} />
---
+++ src/page-sections/home/ProfileCards.tsx
@@ -0,0 +1,39 @@
+import { Badge, BadgeVariant, CardImage } from "@/components";
+import { richTextParser } from "@/utils";
+import wording from "@/wording";
+
+export default function ProfileCards() {
+  return (
+    <section className="fr-container-fluid fr-py-10w">
+      <div className="fr-container">
+        <h2>{richTextParser(wording.homepage.profile_cards.title)}</h2>
+        <div className="fr-grid-row fr-grid-row--gutters flex flex-col md:flex-row">
+          {wording.homepage.profile_cards.cards.map((card, card_index) => (
+            <div className="fr-col-12 fr-col-md-6 flex-1" key={card_index}>
+              <ul className="fr-raw-list fr-badges-group fr-mb-3w flex flex-wrap gap-4">
+                {card.badges.map((badge, index) => (
+                  <li key={index}>
+                    <Badge
+                      label={badge.label}
+                      variant={BadgeVariant[badge.variant]}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <CardImage
+                description={richTextParser(card.subtitle)}
+                image={card.image}
+                title={card.title}
+              />
+              <ul className="fr-raw-list fr-badges-group fr-mb-3w flex flex-wrap gap-4">
+                {card.points.map((point, index) => (
+                  <li key={index}>{point}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
