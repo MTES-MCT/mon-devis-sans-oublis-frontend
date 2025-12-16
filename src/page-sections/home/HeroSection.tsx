64b2d988a6b52beb972e396df8e435d8298fe6fc
Samir BENFARES <sbenfares@gmail.com>
2025-12-16 15:55:44 +0100

    feat: hero section

--- src/page-sections/home/HeroSection.tsx
+++ src/page-sections/home/HeroSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-import { Badge, BadgeVariant, Link } from "@/components";
 import wording from "@/wording";
+import Link from "next/link";
 
 export default function HeroSection() {
   return (
@@ -10,32 +10,49 @@ export default function HeroSection() {
         <div className="flex flex-col md:flex-row md:justify-between justify-center md:items-start items-center">
           <div className="fr-col-12 fr-col-md-5">
             <h1>{wording.homepage.hero_section.title}</h1>
-            <h5 className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">{wording.homepage.hero_section.subtitle}</h5>
+            <p className=" fr-mb-2w fr-mt-1w md:fr-mb-0 md:fr-mt-0">
+              {wording.homepage.hero_section.subtitle}
+            </p>
             <ul className="fr-raw-list fr-badges-group fr-mb-3w flex flex-wrap gap-4">
               {wording.homepage.hero_section.badges.map((badge, index) => (
                 <li key={index}>
-                  <Badge
-                    label={badge.label}
-                    variant={BadgeVariant.BLUE_LIGHT}
-                  />
+                  <p className="fr-badge fr-badge--green-menthe">
+                    {badge.label}
+                  </p>
                 </li>
               ))}
             </ul>
-            <Link {...wording.homepage.check_quote_button} />
-            <h5 className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">{wording.homepage.hero_section.or}</h5>
-            <h5 className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">
-              <Link {...wording.homepage.hero_section.email_button} />
-            </h5>
-            <h5 className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">{wording.homepage.hero_section.email_explanation}</h5>
+            <Link
+              type="button"
+              href={wording.homepage.check_quote_button.href}
+              className="fr-btn fr-btn--lg fr-icon-arrow-right-line fr-btn--icon-right"
+            >
+              {wording.homepage.check_quote_button.label}
+            </Link>
+            <p className="fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">
+              {wording.homepage.hero_section.or}
+            </p>
+            <strong>
+              <Link
+                id="link-16"
+                href={wording.homepage.hero_section.email_button.href}
+                target="_self"
+                className="fr-link fr-icon-mail-fill fr-link--icon-left fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0"
+              >
+                {wording.homepage.hero_section.email_button.label}
+              </Link>
+            </strong>
+            <p className="fr-text--sm fr-mb-1w fr-mt-1w md:fr-mb-0 md:fr-mt-0">
+              {wording.homepage.hero_section.email_explanation}
+            </p>
           </div>
-          <div className="mt-10 md:mt-0 flex justify-center relative h-[250px] md:h-[300px] lg:h-[376px] w-full max-w-[484px]">
+          <div className="flex justify-center">
             <Image
               alt={wording.homepage.hero_section.image.alt}
-              className="object-contain"
-              fill
               priority
-              quality={85}
-              sizes="(max-width: 768px) 272px, (max-width: 1024px) 484px, 100vw"
+              quality={95}
+              width={550}
+              height={550}
               src={wording.homepage.hero_section.image.src}
             />
           </div>
