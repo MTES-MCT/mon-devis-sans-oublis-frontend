b0c7c89ab7be3936302fe16a035a3136f9f9e433
Martin <admin@evlaa.com>
2026-04-09 17:25:46 +0200

    Close (#236)
    * close
    
    * fix: move TypeScript to dependencies for production build
    
    fix: move TypeScript to dependencies + update lock file
    
    ---------
    
    Co-authored-by: Martin Letellier (Beta.gouv) <dinum-327867@Host-001.lan>
    Co-authored-by: Samir BENFARES <sbenfares@gmail.com>

--- src/wording/index.ts
+++ src/wording/index.ts
@@ -1,11 +1,1 @@
-import wording from "./wording.json";
-
-// Automatically update the notice on the homepage based on the latest update card
-const last_update = wording.homepage.last_updates.last_update_cards[0];
-wording.layout.notice = {
-  ...wording.layout.notice,
-  description: last_update.title || wording.layout.notice.description,
-  title: last_update ? "Nouveautés en ligne !" : wording.layout.notice.title
-};
-
-export default wording;
+export { default } from "./wording.json";
